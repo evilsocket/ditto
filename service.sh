a6fd353b66893ea1bb346eccad7ff6247531dc3c
Simone Margaritelli <evilsocket@gmail.com>
2021-02-01 12:45:22 +0100

    misc: small fix or general refactoring i did not bother commenting

--- service.sh
+++ service.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-echo "starting monitor on $1 with email $2 ..."
+OUTPUT="$HOME/ditto_data/$1"
+
+echo "starting monitor on $1 with email $2 (saving on $OUTPUT) ..."
+
 ditto -domain "$1" \
    -monitor 1h \
-   -changes "$HOME/ditto_data/$1" \
+   -changes "$OUTPUT" \
    -keep-changes \
    -ignore-ip-changes \
    -ignore-host-changes \
